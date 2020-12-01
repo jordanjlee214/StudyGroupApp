af4470ceb86d09a326c651bb58bec56fce99daa4
jordanjlee214 <jordanjlee@me.com>
2020-12-01 15:18:10 -0500

    Successfully was able to finish SetupActivity to store user data into the Realtime Database. Now, I need to figure out how to set up profile pics and store them.

--- app/src/main/java/com/leejordan/studygroupapp/SetupActivity.java
+++ app/src/main/java/com/leejordan/studygroupapp/SetupActivity.java
@@ -1,7 +1,10 @@
 package com.leejordan.studygroupapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,19 +12,26 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SetupActivity extends AppCompatActivity {
+
+    //STILL NEED TO MAKE SURE USERNAME IS UNIQUE
 
     private EditText firstName, lastName, username, gender, birthday, school;
     private Button finishSetup;
     private CircleImageView profile;
     private FirebaseAuth mAuth;
     private DatabaseReference usersRef;
+    private ProgressDialog loadingBar;
 
     private String currentUserID;
 
@@ -39,9 +49,10 @@ public class SetupActivity extends AppCompatActivity {
         finishSetup = findViewById(R.id.finishSetupButton);
         profile = findViewById(R.id.setup_profilePic);
         mAuth = FirebaseAuth.getInstance();
-        usersRef = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserID);
-        currentUserID = mAuth.getCurrentUser().getUid();
 
+        currentUserID = mAuth.getCurrentUser().getUid();
+        usersRef = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserID);
+        loadingBar = new ProgressDialog(this);
 
         finishSetup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,6 +97,34 @@ public class SetupActivity extends AppCompatActivity {
         }
         //it is correctly input
         else{
+            loadingBar.setTitle("Setting up your account data...");
+            loadingBar.setMessage("Please wait for a moment as we save your user data...");
+            loadingBar.show();
+            loadingBar.setCanceledOnTouchOutside(true);
+
+            HashMap userData = new HashMap();
+            userData.put("username", u);
+            userData.put("first_name", fN);
+            userData.put("last_name", lN);
+            userData.put("gender", g);
+            userData.put("birthday", b);
+            userData.put("school", s);
+            userData.put("bio", "Hey there! Let's study together!"); //default bio for profiles
+            usersRef.updateChildren(userData).addOnCompleteListener(new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull Task task) {
+                    if(task.isSuccessful()){
+                        loadingBar.dismiss();
+                        Toast.makeText(SetupActivity.this, "Your account has been successfully set up.", Toast.LENGTH_LONG).show();
+                        sendMain();
+                    }
+                    else{
+                        loadingBar.dismiss();
+                        String message = task.getException().getMessage();
+                        Toast.makeText(SetupActivity.this, "Uh oh! An error occurred: " + message, Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
 
 
         }
@@ -103,5 +142,12 @@ public class SetupActivity extends AppCompatActivity {
         }
         return true;
 
+    }
+
+    private void sendMain(){
+        Intent sendToMain = new Intent(SetupActivity.this, MainActivity.class);
+        sendToMain.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(sendToMain);
+        finish();
     }
 }
